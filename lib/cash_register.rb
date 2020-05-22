5ba15028ec0cbbdee9d28111bdc17f7e4d842828
Jung Lee <lee.jung.s95@gmail.com>
2020-05-22 01:57:59 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,44 @@
+require 'pry'
+class CashRegister
+  attr_accessor :discount, :total, :items, :transaction
 
+
+
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+    @items = []
+    @transaction = []
+  end
+
+  def add_item(title, price, quantity = 1)
+    @transaction << price
+    quantity.times {@items << title}
+    if quantity > 1
+      @total = @total + (price*quantity)
+    else
+      @total = @total + price
+    end
+  end
+
+  def apply_discount
+    @discount = @discount*0.01
+    @total = @total - (@total*@discount)
+    if @discount == 0
+      return "There is no discount to apply."
+    else
+      return "After the discount, the total comes to $#{@total.floor}."
+    end
+  end
+
+  def void_last_transaction
+    @transaction.each do |price|
+      @total = @total - price
+    end
+  end
+
+
+
+
+
+end
